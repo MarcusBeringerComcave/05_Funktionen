12340902a989d7fcdf58e47ef6121c0c88eb2323
Marcus Beringer <marcusberinger-comcave@web.de>
2022-11-11 10:05:29 +0100

    Funktionsaufruf

--- script_01.js
+++ script_01.js
@@ -2,3 +2,23 @@
 
 /***** Funktionen 01 *****/
 
+
+// console.log("Hallo Marcus!");
+
+
+// 1. Kabselung von Codeblöcken //
+
+
+
+// Funktionsaufruf (call) //
+test();
+
+// Funktionsrumpf (body) | callee  Funktionsdeklaration//
+
+function test()
+{
+    console.log("Hallo Marcus !");
+
+}
+
+
