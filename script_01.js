8330b45e9063de3bbbbb2fffc4fa471a3593635a
Peter <schmidt.multimedia@gmail.com>
2022-11-11 10:05:10 +0100

    Funktionen 01

--- script_01.js
+++ script_01.js
@@ -2,3 +2,17 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Sabine!");
+}
+
+
+
